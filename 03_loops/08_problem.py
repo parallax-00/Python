b7349246bf95f3c3a8041e7f62c8eacc5d090462
parallax70 <155736682+parallax70@users.noreply.github.com>
2025-05-23 10:11:44 +0530

    push

---
+++ 03_loops/08_problem.py
@@ -0,0 +1,9 @@
+number = int(input("Enter the number to be checked: "))
+is_prime=True
+
+if (number>1):
+    for i in range(2,number):
+        if( number%i==0 ): 
+            is_prime=False
+            break
+print(is_prime)
