9ca4cda64e9335c7a19acffc305e99832feb7c95
parallax70 <155736682+parallax70@users.noreply.github.com>
2025-05-24 07:17:56 +0530

    push

---
+++ 04_functions/02_problem.py
@@ -0,0 +1,7 @@
+def sum_of_two(a,b):
+    return (a+b)
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+# summation=sum_of_two(num1,num2)
+# print(f"Sum of the two given numbers is: {summation}")
+print("Sum of the two givern numbers is: ", sum_of_two(num1,num2))
